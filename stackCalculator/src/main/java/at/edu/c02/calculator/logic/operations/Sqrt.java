4c8b3bb6023f45510e31dc94bc533275b2078a93
Dinu Stetco <dinu.stetco@edu.campus02.at>
2019-01-12 10:43:31 +0100

    SQRT

---
+++ stackCalculator/src/main/java/at/edu/c02/calculator/logic/operations/Sqrt.java
@@ -0,0 +1,16 @@
+//Dinus Class
+package at.edu.c02.calculator.logic.operations;
+
+import at.edu.c02.calculator.CalculatorException;
+import at.edu.c02.calculator.MathOperation;
+
+public class Sqrt implements MathOperation{
+
+	@Override
+	public double calculate(double a, double b) throws CalculatorException {
+		// TODO Auto-generated method stub
+		return Math.sqrt(a+b);
+	}
+
+
+}
